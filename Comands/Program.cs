99e872bee25c2d9eab271670c961879b608b6aa1
tonyathb <tonya_thb@abv.bg>
2025-03-05 00:41:44 +0200

    Commands Insert Remove from array

---
+++ Comands/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comands
+{
+    class Program
+    {
+        static List<string> Reverse(List<string> arr)
+        {
+            //list otzad napred
+            List<string> result = new List<string>(); //null
+            for (int i = arr.Count - 1; i >= 0; i--)
+            {
+                result.Add(arr[i]);
+            }
+            return result;
+        }
+
+        static List<string> Distinct(List<string> arr)
+        {
+            //list otzad napred
+            List<string> result = new List<string>(); //null
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (!result.Contains(arr[i]))
+                {
+                    result.Add(arr[i]);
+                }
+            }
+            return result;
+        }
+
+        static void Repleace(List<string> arr, int index, string newElement)
+        {
+            if (index < arr.Count)
+            {
+                arr[index] = newElement;
+            }
+        }
+
+        //static void Print(List)
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            List<string> output = new List<string>(input);
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                switch (command[0])
+                {
+                    case "Distinct": output = Distinct(output); break;
+                    case "Add":
+                        {
+                            int ind = int.Parse(command[1]);
+                            string value = command[2];
+                            output.Insert(ind, value);
+                            break;
+                        }
+                    case "Reverse": output = Reverse(output); break;
+                    case "Repleace":
+                        {
+                            int ind = int.Parse(command[1]);
+                            string value = command[2];
+                            Repleace(output, ind, value); 
+                            break;
+                        }
+                    case "Remove":
+                        {
+                            int index = int.Parse(input[1]);
+                            output.RemoveAt(index);
+                            break;
+                        } 
+                }
+                Console.WriteLine(string.Join(" ", output));
+            }
+
+        }
+    }
+}
---
+++ InsertRemoveInArray/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InsertRemoveInArray
+{
+    class Program
+    {
+        static string[] RemoveAt(string[] input, int index)
+        {
+            if (index < 0 || index > input.Length - 1)
+            {
+                return input;
+            }
+            string[] output = new string[input.Length - 1];
+            for (int i = 0; i < index; i++)
+            {
+                output[i] = input[i];
+            }
+            for (int i = index; i < input.Length - 1; i++)
+            {
+                output[i] = input[i + 1];
+            }
+            return output;
+        }
+        static string[] Insert(string[] arr, int index, string newElement)
+        {
+            string[] res = new string[arr.Length + 1];
+            if (index < 0 || index > arr.Length)
+            {
+                return arr;
+            }
+
+            res = new string[arr.Length + 1];
+            for (int i = 0; i < index; i++)
+            {
+                res[i] = arr[i];
+            }
+            res[index] = newElement;
+            for (int i = index + 1; i < arr.Length; i++)
+            {
+                res[i] = arr[i - 1];
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            string[] command = Console.ReadLine().Split().ToArray();
+            while (command[0].ToUpper() != "END")
+            {
+                string[] output=input;
+                switch (command[0])
+                {
+                    case "Insert":
+                        {
+                            int ind = int.Parse(command[1]);
+                            string element = command[2];
+                            output = Insert(input, ind, element);
+                            break;
+                        }
+                    case "RemoveAt":
+                        {
+                            int nom = int.Parse(command[1]);
+                            output = RemoveAt(input, nom);
+                            break;
+                        }
+                }
+
+                //////if (command[0] == "Insert")
+                //////{
+                //////    int ind = int.Parse(command[1]);
+                //////    string element = command[2];
+                //////    output = Insert(input, ind, element);
+                //////    //if (ind < 0 || ind > input.Length)
+                //////    //{
+                //////    //    continue;
+                //////    //}
+
+                //////    //output = new string[input.Length + 1];
+                //////    //for (int i = 0; i < ind; i++)
+                //////    //{
+                //////    //    output[i] = input[i];
+                //////    //}
+                //////    //output[ind] = element;
+                //////    //for (int i = ind + 1; i < output.Length; i++)
+                //////    //{
+                //////    //    output[i] = input[i - 1];
+                //////    //}
+                //////    Console.WriteLine(string.Join(", ", output));
+                //////    Console.WriteLine("----------------Input new command------------------");
+                //////    command = Console.ReadLine().Split().ToArray();
+                //////    input = output;
+                //////}
+
+                //////if (command[0] == "RemoveAt")
+                //////{
+
+                //////    int nom = int.Parse(command[1]);
+                //////    output = RemoveAt(input, nom);
+
+                //////    //output = new string[input.Length - 1];
+                //////    //for (int i = 0; i < ind; i++)
+                //////    //{
+                //////    //    output[i] = input[i];
+                //////    //}
+                //////    //for (int i = ind; i < input.Length - 1; i++)
+                //////    //{
+                //////    //    output[i] = input[i + 1];
+                //////    //}
+                //////    Console.WriteLine(string.Join(", ", output));
+                //////    Console.WriteLine("----------------Input new command------------------");
+                //////    command = Console.ReadLine().Split().ToArray();
+                //////    input = output;
+                //////    continue;
+                //////}
+
+                Console.WriteLine(string.Join(", ", output));
+                Console.WriteLine("----------------Input new command------------------");
+                command = Console.ReadLine().Split().ToArray();
+                input = output;
+
+            }
+        }
+    }
+}
